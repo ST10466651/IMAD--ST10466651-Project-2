abf26c580f0e3b67e8427df11460beeb458d1e43
ST10466651 <ST10466651@vcconnect.edu.za>
2025-05-20 10:52:23 +0200

    First Page Code

--- app/src/main/java/za/co/varsitycollege/st10466651/brain_bank/MainActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10466651/brain_bank/MainActivity.kt
@@ -1,10 +1,26 @@
 package za.co.varsitycollege.st10466651.brain_bank
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
+val arrQuestions = arrayOf("A basketball game consists of four quarters of 12 minutes each in the NBA.",
+    "Cristiano Ronaldo has only played for clubs in Spain during his professional career.",
+    "In tennis, a score of zero is called 'love.'",
+    "The Olympic Games are held every 3 years.",
+    "South Africa won the Rugby World Cup in both 1995 and 2019.")
+
+val arrAnswer = arrayOf(true,
+    false,
+    true,
+    false,
+    true)
+
+val arrUserAnswer = BooleanArray(5)
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +32,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val start = findViewById<Button>(R.id.btnStart)
+
+        start.setOnClickListener {
+            val intent = Intent(this, Quiz::class.java)
+            startActivity(intent)
+        }
+
+        val intent = Intent(this, Quiz::class.java).apply {
+            putExtra("Questions_Array", arrQuestions) // Add data to pass
+            putExtra("Answer_Array", arrAnswer) // Add data to pass
+            putExtra("UserAnswer_Array", arrUserAnswer) // Add data to pass
+        }
+        startActivity(intent)
     }
 }
